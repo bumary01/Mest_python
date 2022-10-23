cec5bde0cec5de3dd1976155241885809315876d
Bunmi Mary <bumary2016@gmail.com>
2022-10-24 00:14:25 +0100

    this is my second commit

--- for_while.py
+++ for_while.py
@@ -23,7 +23,7 @@ Total_Amount= float(50000)
 Marketing =(0.25 * float(Total_Amount))
 Operational_Expenses = (0.5* float(Total_Amount))
 Total_customer_Aquisition = (0.25* float(Total_Amount))
-Financial_statement = (f"Hello {Name}!\n your financial statement is as follows:\n Marketing Expenses = {Marketing}\n Operational Expenses = {Operational_Expenses}\n Customer Aquisition = {Total_customer_Aquisition}\n Total amount allocated = {Total_Amount}" )
+Financial_statement = (f"Hello {Name}!\n your capstone financial statement is as follows:\n Marketing Expenses = GHC {Marketing}\n Operational Expenses = GHC {Operational_Expenses}\n Customer Aquisition = GHC {Total_customer_Aquisition}\n Total amount allocated = GHC {Total_Amount}" )
 print (Financial_statement)
 
 Max_possible_customer= (Total_customer_Aquisition/5)
